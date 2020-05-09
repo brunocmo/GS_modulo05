7dd188ebeedd8d9c806a6fcca88a644af1063c1f
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-05-08 21:17:35 -0300

    Models Relations

--- src/database/migrations/1588979360091-CreateUsers.ts
+++ src/database/migrations/1588979360091-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1588979360091 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
