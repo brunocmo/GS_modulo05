af2db21de9d90492b5caa249240c46d340ce2e0d
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-08-17 23:01:49 -0300

    List providers

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -2,6 +2,8 @@ import { uuid } from 'uuidv4';
 
 import IUsersRepository from '@modules/users/repositories/IUserRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
+
 
 import User from '../../infra/typeorm/entities/User';
 
@@ -20,6 +22,15 @@ export default class FakeUserRepository implements IUsersRepository {
 
     return findUser;
 
+  }
+
+  public async findAllProviders({ except_user_id }: IFindAllProvidersDTO): Promise<User[]> {
+    let { users } = this;
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+    return users;
   }
 
 
